6a4a28b38e47c581f1d58dceaf28473653b334dc
cryosteam <g.korenblyum@gmail.com>
2025-01-21 10:41:25 +0000

    fix: db host name

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'db'),
+        'HOST': os.getenv('DB_HOST', 'db_fg'),
         'PORT': os.getenv('DB_PORT', 5432),
     }
 }
